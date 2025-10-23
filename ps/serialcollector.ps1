d391700481f05f7a6bb05816ae38c634fc02fb74
quickpots <102370841+quickpots@users.noreply.github.com>
2025-10-23 16:28:27 +0100

    Create serialcollector.ps1

---
+++ ps/serialcollector.ps1
@@ -0,0 +1,29 @@
+Clear-Host
+Write-Host @"
+   ____        _      _    _____      _       
+  / __ \      (_)    | |  |  __ \    | |      
+ | |  | |_   _ _  ___| | _| |__) |__ | |_ ___ 
+ | |  | | | | | |/ __| |/ /  ___/ _ \| __/ __|
+ | |__| | |_| | | (__|   <| |  | (_) | |_\__ \
+  \___\_\\__,_|_|\___|_|\_\_|   \___/ \__|___/
+                                              
+                                              
+"@ -ForegroundColor Blue										
+
+$outputFile = 'c:\serial.txt'
+$cmds = @(
+    'wmic os get serialnumber'
+	'wmic cpu get processorid'
+	'wmic os get installdate'
+	'wmic memorychip get serialnumber'
+)
+	
+foreach ($cmd in $cmds) {
+	"command used: $cmd" | Out-File -FilePath $outputFile -Append
+    $result = Invoke-Expression $cmd
+
+    $result | Out-File -FilePath $outputFile -Append
+    ("=" * 50) | Out-File -FilePath $outputFile -Append  # Separate results for readability
+}
+Write-Host "Serials saved to c:\serial.txt"
+notepad.exe c:\serial.txt
